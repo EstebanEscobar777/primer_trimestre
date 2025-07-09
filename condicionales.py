3a08a85f999d7ab51156d8d0bcbca0fbd1075129
Esteban Escobar <eescobarr@paulovi.edu.co>
2025-07-09 17:27:18 -0500

    actividad condicionales

---
+++ condicionales.py
@@ -0,0 +1,50 @@
+#verifica si un numero es positivo, negativo o cero.
+# num1=float(input("ingrese el numero que desee: "))
+# if num1 > 0:
+#     print("positivo")
+# elif num1 < 0:
+#     print("es negativo")
+# else:
+#     print("es cero")
+
+#calcula el mayor de dos numeros ingresados 
+
+# num1=float(input("ingrese el primer numero: "))
+# num2=float(input("ingrese el segundo numero: "))
+# if num1>num2:
+#     print(F"el numero {num1} es mayor que {num2}")
+# elif num2<num1:
+#     print(F"el numero {num2} es mayor que {num1}")
+
+#determina si un numero es par o impar
+
+# num1=float(input("ingrese el numero que desee: "))
+# if num1 == 0:
+#     print ("el numero es cero y es par")
+# elif num1 % 2 == 0:
+#     print("el numero es par")
+# else:
+#     print("el numero es impar")
+
+#4 dado un numero, verifica si esta entre 10 y 20.
+
+# num1=float(input("ingrese el numero que desee: "))
+# if 10 <= num1 <= 20:
+#     print("el numero esta entre 10 y 20 ")
+# else:
+#     print("el numero no esta entre 10 y 20")
+
+#5 dados tres numeros encuentra el mayor usando condicionales 
+
+num1=int(input("ingrese el primer numero: "))
+num2=int(input("ingrese el segundo numero: "))
+num3=int(input("ingrese el tercer numero: "))
+
+if num1 >= num2 and num1 >=num3:
+    print(f"el numero mayor es : {num1}")
+elif num2 >= num1 and num2 >= num3:
+    print(f"el numero mayor es: {num2}")
+else:
+    print(f"el numero mayor es: {num3}")
+
+#6
